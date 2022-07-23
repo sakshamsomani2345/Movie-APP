c2ea5dc8176a5dd471f97fc2e5a2857220f01408
sakshamsomani2345 <sakshamsomani2345@gmail.com>
2022-07-23 13:03:40 +0530

    movie

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,72 +1,95 @@
 import React, { Component } from "react";
 // import { movies } from "./getMovies";
 import axios from "axios";
+import Banner from "./Banner";
 export default class Movies extends Component {
   constructor() {
     super();
     this.state = {
       hover: "",
       parr: [1],
-      currpage:1,
-      movies:[]
+      currpage: 1,
+      movies: [],
+      favourites: [],
     };
   }
   async componentDidMount() {
-    const res= await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5ce2d3feb38f64f74c94208daf524e32&language=en-US&page=${this.state.currpage}`);
-    const data=res.data;
-this.setState({
-    movies:[...data.results]
-
-})
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=5ce2d3feb38f64f74c94208daf524e32&language=en-US&page=${this.state.currpage}`
+    );
+    const data = res.data;
+    this.setState({
+      movies: [...data.results],
+    });
   }
-  async changemovies(){
+  async changemovies() {
     console.log(this.state.currpage);
-    const res= await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5ce2d3feb38f64f74c94208daf524e32&language=en-US&page=${this.state.currpage}`);
-    const data=res.data;
-this.setState({
-    movies:[...data.results]
-   })
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=5ce2d3feb38f64f74c94208daf524e32&language=en-US&page=${this.state.currpage}`
+    );
+    const data = res.data;
+    this.setState({
+      movies: [...data.results],
+    });
   }
-   handleright=()=>{
-    let temparr=[];
-for (let index = 1; index < this.state.parr.length+1; index++) {
-temparr.push(index);    
-}
-this.setState({
-    parr:[...temparr],
-    currpage:this.state.currpage+1,
-},
-this.changemovies
-
-)
-   }
-   handleleft=()=>{
-    if(this.state.currpage!=1){
-        this.setState({
-            currpage:this.state.currpage-1,
+  handleright = () => {
+    let temparr = [];
+    for (let index = 1; index < this.state.parr.length + 1; index++) {
+      temparr.push(index);
+    }
+    this.setState(
+      {
+        parr: [...temparr],
+        currpage: this.state.currpage + 1,
+      },
+      this.changemovies
+    );
+  };
+  handleleft = () => {
+    if (this.state.currpage != 1) {
+      this.setState(
+        {
+          currpage: this.state.currpage - 1,
         },
         this.changemovies
-        
-        )
+      );
     }
-    
-    
-    
-   }
-   handleclick=(value)=>{
-if(value!=this.state.currpage){
+  };
+  handleclick = (value) => {
+    if (value != this.state.currpage) {
+      this.setState(
+        {
+          currpage: value,
+        },
+        this.changemovies
+      );
+    }
+  };
+  handlefavourite = (movie) => {
+    let oldata = JSON.parse(localStorage.getItem("movies") || "[]");
+    if (this.state.favourites.includes(movie.id)) {
+      oldata = oldata.filter((m) => m.id != movie.id);
+    } else {
+      oldata.push(movie);
+    }
+    localStorage.setItem("movies", JSON.stringify(oldata));
+    console.log(oldata);
+    this.handlestatefav();
+  };
+  handlestatefav = () => {
+    let oldata = JSON.parse(localStorage.getItem("movies") || "[]");
+
+    let temp = oldata.map((movie) => movie.id);
     this.setState({
-        currpage:value
-    },
-    this.changemovies
-    )
-}
-   }
+      favourites: [...temp],
+    });
+  };
   render() {
     // let movie = movies.results;
 
     return (
       <>
+        <Banner />
         {this.state.movies.length == 0 ? (
           <div className="spinner-border text-primary" role="status">
             <span className="visually-hidden">Loading...</span>
@@ -101,8 +124,14 @@ if(value!=this.state.currpage){
                       }}
                     >
                       {this.state.hover == movieobj.id && (
-                        <a className="btn btn-primary movies-button">
-                          Add to favourites
+                        <a
+                          onClick={() => this.handlefavourite(movieobj)}
+                          className="btn btn-primary movies-button"
+                        >
+                          {
+                            this.state.favourites.includes(movieobj.id)?"Remove":"Add"
+                          }
+                          {/* Add to favourites */}
                         </a>
                       )}
                     </div>
@@ -121,12 +150,19 @@ if(value!=this.state.currpage){
               <nav aria-label="Page navigation example">
                 <ul className="pagination">
                   <li className="page-item">
-                    <a className="page-link" onClick={this.handleleft}>Previous</a>
+                    <a className="page-link" onClick={this.handleleft}>
+                      Previous
+                    </a>
                   </li>
 
                   {this.state.parr.map((value) => (
                     <li className="page-item">
-                      <a className="page-link" onClick={()=>this.handleclick(value)}>{value}</a>
+                      <a
+                        className="page-link"
+                        onClick={() => this.handleclick(value)}
+                      >
+                        {value}
+                      </a>
                     </li>
                   ))}
                   <li className="page-item">
